d2f6062b505b656c713c203ca1d8c60cb4b5f200
Masayuki Tanaka <masayuki0812@mac.com>
2012-11-17 22:00:26 +0900

    Add IP conversion in Main

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
         try {
             // CIDR
             String cidr = "192.168.0.0/16";
-            System.out.println("## Convert " + cidr);
             String[] ips = CIDR.convertToRange(cidr);
-            System.out.println("## -> " + ips[0] + " ~ " + ips[1]);
+            System.out.println("## Converted " + cidr + " to '" + ips[0] + " ~ " + ips[1] + "'");
 
-            
+            // IP
+            String ip = "192.168.1.1";
+            long nIp = IP.toLong(ip);
+            System.out.println("## Converted " + ip + " to '" + nIp + "'");
         }
         catch (Exception e) {
             e.printStackTrace();
