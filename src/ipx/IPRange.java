a780a797935b49186986ea6f7db5e3b15ca64a26
Masayuki Tanaka <masayuki0812@mac.com>
2012-11-18 15:42:06 +0900

    Add methods to IPRange

--- src/ipx/IPRange.java
+++ src/ipx/IPRange.java
@@ -5,14 +5,30 @@ public class IPRange {
     private IP start;
     private IP end;
 
+    private String id;
+
     public IPRange(String start, String end) throws Exception {
         this.start = new IP(start);
         this.end = new IP(end);
+        this.id = null;
+    }
+    public IPRange(String start, String end, String id) throws Exception {
+        this.start = new IP(start);
+        this.end = new IP(end);
+        this.id = id;
+    }
+
+    public IP getStart() {
+        return this.start;
+    }
+
+    public IP getEnd() {
+        return this.end;
     }
 
     @Override
     public String toString() {
-        return this.start + "-" + this.end;
+        return this.start + "-" + this.end + ":" + this.id;
     }
 
     public boolean includes(IP ip) {
